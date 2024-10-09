f292d4dd1d747d546e4ce94593c6e2c3c942a9f2
Dwe <ducduyhoang5@gmail.com>
2024-10-09 17:31:08 +0700

    Add PD controller for the SMC model without the lifting Unit

--- SMSC_XLU_Modeling_PID.m
+++ SMSC_XLU_Modeling_PID.m
@@ -6,30 +6,28 @@ clear;
 %--------------------------------------------------------------------------
 
 % Thiết lập các thông số cho dầm
-L = 0.63; EI = 0.754; rho_A = .297;
+L = 0.63; EI = 0.754; rho_A = 0.297;
 % Các vật nặng 
 mw = 13.1; mk = 0.04; mh = 0.86; g = 9.81;
 
 % Thiết lập thông số không gian và thời gian
-n = 9; r = 10000;
-tmax = 15;
+n = 9; r = 20000;
+tmax = 30;
 delta_Y = L/(n - 1); % Bước không gian
 delta_t = tmax/(r - 1); % Bước thời gian
 
+% Bộ thông số PID
+kp1 = 0.1; ki1 = 3; kd1 = 9; % Hệ số 3 khâu PID cho xe con
+I = 0; % Khởi tạo giá trị tính tích phân
+sp1 = 1; % Giá trị đặt cho xe con
+e1 = zeros(1,r); % Sai số vị trí xe con
+
 % Khởi tạo ma trận để lưu giá trị
 w = zeros(n,r);
-x3 = zeros(1,r); % Độ lắc của thanh tại x2 (x3)
-x2 = 2; 
-dx2dt_2 = zeros(1,r);
-dx2dt_2(1:2) = delta_Y;
 
 % Lực tác động vào xe con
 F1 = zeros(1,r);
-F1(1:r) = 10;
-
-% Lực tác động vào xe nâng
-F2 = zeros(1,r);
-F2(1:r) = 10;
+F1(1:r/2) = 1;
 
 %--------------------------------------------------------------------------
 for j = 3:(r - 1)
@@ -39,39 +37,21 @@ for j = 3:(r - 1)
     for i = 3:(n - 2)
         % Đạo hàm theo Y
         wyyyy = (w(i + 2,j) - 4*w(i + 1,j) + 6*w(i,j) - 4*w(i - 1,j) + w(i - 2,j));
-        S1 = (-EI/(rho_A*delta_Y^4))*wyyyy; 
-        S4 = (-EI/(mh*delta_Y^3))*wyyyy;
-
-        % Chuyển động của xe nâng
-        dx3dt_2 = (x3(j + 1) - 2*x3(j) + x3(j - 1))/(2*delta_t^2);
-        wyx2 = (w(x2 + 1,j - 1) - w(x2 - 1,j - 1))/(2*delta_Y^2);
-        dx2dt_2(j + 1) = 2*dx2dt_2(j) - dx2dt_2(j - 1) + (F2(j + 1) - mh*g - mh*dx3dt_2*wyx2)/mh*delta_t^2; % 5c
-
-        % Cập nhật vị trí x2
-        if dx2dt_2(j + 1) < delta_Y
-            x2 = 2;
-            dx2dt_2(j + 1) = delta_Y;
-        elseif dx2dt_2(j + 1) > L - delta_Y
-            x2 = n - 1;
-            dx2dt_2(j + 1) = L - delta_Y;
-        else
-            x2 = ceil(dx2dt_2(j + 1)/delta_Y);
-        end
-
-        % Độ lắc của thanh
-        if dx2dt_2(j + 1) ~= x2
-            w(i,j + 1) = 2*w(i,j) - w(i,j - 1) + delta_t^2*S1; % 5a
-            x3(j + 1) = w(i,j + 1) - w(1,j + 1);
-        else
-            w(i,j + 1) = 2*w(i,j) - w(i,j - 1) + delta_t^2*S4; % 5d
-            x3(j + 1) = w(i,j + 1) - w(1,j + 1);
-        end
+        S1 = (-EI/(rho_A*delta_Y^4))*wyyyy;
+        w(i,j + 1) = 2*w(i,j) - w(i,j - 1) + delta_t^2*S1; % 5a
     end
     wyyyl = (-2*w(n,j) + 3*w(n - 1,j) - w(n - 2,j))/(2*delta_Y^3);
     S3 = (EI/mk)*wyyyl;
     w(2,j + 1) = w(1,j + 1);
     w(n,j + 1) = 2*w(n,j) - w(n,j - 1) + delta_t^2*S3; % 5e
     w(n - 1,j + 1) = (w(n,j + 1) + w(n - 2,j + 1))/2;
+%--------------------------------------------------------------------------
+%                       Bộ điều khiển PD cho xe con
+%--------------------------------------------------------------------------
+    e1(j + 1) = sp1 - w(1,j);
+    I = I + e1(j + 1)*delta_t;
+    D = (e1(j + 1) - e1(j))/delta_t;
+    F1(j + 2) = kp1*e1(j + 1) + kd1*D;
 end
 %--------------------------------------------------------------------------
 
@@ -79,7 +59,8 @@ end
 t_tr = linspace(0,tmax,r);
 
 % Vẽ đồ thị
-subplot(2,2,1);
+figure(1)
+subplot(2,1,1);
 grid on;
 hold on;
 plot(t_tr,w(1,:),'b','LineWidth',1.5);
@@ -87,7 +68,7 @@ title({'Vị trí của xe con'});
 ylabel('Vị trí xe con (m)','FontSize',12);
 xlabel('Thời gian (s)','FontSize',12);
 
-subplot(2,2,2);
+subplot(2,1,2);
 grid on;
 hold on;
 plot(t_tr,w(n,:) - w(1,:),'g','LineWidth',1.5); % Vị trí tương đối của mk so với xe con
@@ -95,18 +76,10 @@ title({'Độ lắc đỉnh thanh'});
 ylabel('Độ lắc so với vị trí cân bằng (m)','FontSize',12);
 xlabel('Thời gian (s)','FontSize',12);
 
-subplot(2,2,3);
+figure(2)
 grid on;
 hold on;
-plot(t_tr,dx2dt_2,'r','LineWidth',1.5);
-title({'Vị trí xe nâng'});
-ylabel('Vị trí xe nâng (m)','FontSize',12);
-xlabel('Thời gian (s)','FontSize',12);
-
-subplot(2,2,4);
-grid on;
-hold on;
-plot(t_tr,x3,'Color',[1 0.5 0],'LineWidth',1.5);
-title({'Độ lắc xe nâng'});
-ylabel('Độ lắc xe nâng (m)','FontSize',12);
+plot(t_tr,F1(1:r),'g','LineWidth',1.5);
+title({'Lực F1'});
+ylabel('Lực F1','FontSize',12);
 xlabel('Thời gian (s)','FontSize',12);
